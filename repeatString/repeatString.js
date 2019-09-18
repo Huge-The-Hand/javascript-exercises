5b149f35354e7fcccf2439df168b50ac2fa10519
Huge-The-Hand <themba.dombo@gmail.com>
2019-09-18 22:09:08 +0200

    completed repeat string function passing all tests

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, times_repeated ) {
+    let addWords = '';
+    if (times_repeated<0){
+        return('ERROR');
+        
+    } else {
+    for (let i = 0; i < times_repeated; i++) {
+        addWords += string;
+    } 
+    return(addWords);
+    }
 
 }
-
+//console.log(repeatString('hey', -2)) //for testing in the terminal
 module.exports = repeatString
