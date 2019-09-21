0cdd48e2fe941858b18afb1f71bb6fdfa299b038
Huge-The-Hand <themba.dombo@gmail.com>
2019-09-21 18:00:09 +0200

    added the loop code to reverse strings

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(userString) {
 
+    let rev_string ; //for some reason this store a NaN string don't know how to remove
+    for (i = 0;i <= userString.length; i++) {
+        rev_string += userString[userString.length-i];
+    }
+    let str = rev_string.substr(3); //to exclude the Nan from beginning of statement
+    return str;
+    
 }
 
+console.log(reverseString("hello"))
 module.exports = reverseString
