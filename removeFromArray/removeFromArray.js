0c91799a05f3e2a5be19078a0616d69b17ef7b17
Huge-The-Hand <themba.dombo@gmail.com>
2019-09-23 22:15:37 +0200

    removed all the commented code

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -13,24 +13,3 @@ const removeFromArray = function() {
 }
 
 module.exports = removeFromArray
-//con = removeFromArray([21,2,3,'jello'],'jello');
-//console.log(con);
-
-
-
-
-//["bar", "baz", "foo", "qux"]
-
-//list.splice(0, 2) 
-// Starting at index position 0, remove two elements ["bar", "baz"] and retains ["foo", "qux"].
-/*
-function longestString() {
-    var longest = '';
-    for (var i=0; i < arguments.length; i++) {
-      if (arguments[i].length > longest.length) {
-        longest = arguments[i];
-      }
-    }
-    return longest;
-  }
-*/
