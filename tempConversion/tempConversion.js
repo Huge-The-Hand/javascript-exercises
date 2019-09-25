30e678086c3db86c61442396a154dc34275b007b
Huge-The-Hand <themba.dombo@gmail.com>
2019-09-25 20:17:12 +0200

    added logic to do the conversions and display one decimal number

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(freinInput) {
+  const freinConverted = (5/9 * (freinInput-32));
+  const FCRounded = Math.round(freinConverted * 10) / 10; //the 100 determines the numbers displayed after the decimal
+  return FCRounded
 }
 
-const ctof = function() {
-
+const ctof = function(celsInput) {
+  const celsiusConv = ((celsInput * 9/5) + 32);
+  const CCRounded = Math.round(celsiusConv * 10) / 10;
+  return CCRounded;
 }
 
+console.log(ctof(32))
+console.log(ftoc(47))
 module.exports = {
   ftoc,
   ctof
